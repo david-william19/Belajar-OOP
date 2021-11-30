11795b091fee6590a1d70a41f3287067e09a7d6b
David William Da Costa <55936252+david-william19@users.noreply.github.com>
2021-11-30 17:33:34 +0700

    first touch

---
+++ JavaProject1/src/main/java/Inheritance/inherit.java
@@ -0,0 +1,22 @@
+package Inheritance;
+
+import java.util.*;
+/**
+ *
+ * @author davdwilliam_
+ */
+public class inherit {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Masukkan angka pertama : ");
+        int a = sc.nextInt();
+        
+        System.out.println("Masukkan angka kedua : ");
+        int b = sc.nextInt();
+        
+        System.out.println("jika dijumlahkan menjadi : "+ (a+b));
+    }
+}

9896d70e34a4a65a34d9d7c8351670cdc6c5b803
David William Da Costa <55936252+david-william19@users.noreply.github.com>
2021-11-30 17:38:21 +0700

    update nih

---
+++ JavaProject1/src/main/java/Thread/contohthread.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Thread;
+
+/**
+ *
+ * @author davdwilliam_
+ */
+public class contohthread {
+    
+}
